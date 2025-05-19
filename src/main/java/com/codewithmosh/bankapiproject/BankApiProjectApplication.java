cdaa1184b9301f794c49bdc1a2cd3518d29d9d0c
Bubacarr Jobarteh <bubisjobs@gmail.com>
2025-05-19 15:42:01 -0300

    newly created Bank Api Project

---
+++ src/main/java/com/codewithmosh/bankapiproject/BankApiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.codewithmosh.bankapiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankApiProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codewithmosh/bankapiproject/BankApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithmosh.bankapiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApiProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
